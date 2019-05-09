538e1097ba0c50501a388d8ef3c5f7b0d4a027ca
apowel01 <44123994+apowel01@users.noreply.github.com>
2019-05-08 21:35:13 -0700

    working assignment 8 in full and clean code

--- amberley_code/main.c
+++ amberley_code/main.c
@@ -1,10 +1,5 @@
 #include "msp.h"
-#include "LCD.h"
-#include "keypad.h"
 #include "delay.h"
-#include "lock.h"
-#include "timer_a.h"
-#include "exec_timing.h"
 #include "dac.h"
 #include "uart.h"
 /**
@@ -13,26 +8,11 @@
 
 void main(void)
 {
-//    char key_str;
+    uint16_t inValue;
     delay_set_dco(FREQ_3_0_MHz);
 
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-#if 0
-    Write_string_LCD("Hello World");
-    while(1) {
-        key_str = keypad_get_ascii();
-        if (key_str != KEYPAD_NOTPRESSED) {
-            Clear_LCD();
-            Write_char_LCD(key_str);
-        }
-        _delay_cycles(25000);
-    }
-#else
-//    timer_a_init();
-//    lock_main();
-//    exec_timing_main();
-    uint16_t inValue;
     dac_init();
     uart_init();
 
@@ -47,6 +27,4 @@ void main(void)
             uart_put_str("ERROR: number out of range");
         }
     }
-
-#endif
 }
--- amberley_code/uart.c
+++ amberley_code/uart.c
@@ -5,24 +5,19 @@
  *      Author: Amberley
  */
 #include "msp.h"
-#if 0
-typedef struct {
-  __IO uint16_t CTLW0;                                                           /*!< eUSCI_Ax Control Word Register 0 */
-  __IO uint16_t CTLW1;                                                           /*!< eUSCI_Ax Control Word Register 1 */
-       uint16_t RESERVED0;
-  __IO uint16_t BRW;                                                             /*!< eUSCI_Ax Baud Rate Control Word Register */
-  __IO uint16_t MCTLW;                                                           /*!< eUSCI_Ax Modulation Control Word Register */
-  __IO uint16_t STATW;                                                           /*!< eUSCI_Ax Status Register */
-  __I  uint16_t RXBUF;                                                           /*!< eUSCI_Ax Receive Buffer Register */
-  __IO uint16_t TXBUF;                                                           /*!< eUSCI_Ax Transmit Buffer Register */
-  __IO uint16_t ABCTL;                                                           /*!< eUSCI_Ax Auto Baud Rate Control Register */
-  __IO uint16_t IRCTL;                                                           /*!< eUSCI_Ax IrDA Control Word Register */
-       uint16_t RESERVED1[3];
-  __IO uint16_t IE;                                                              /*!< eUSCI_Ax Interrupt Enable Register */
-  __IO uint16_t IFG;                                                             /*!< eUSCI_Ax Interrupt Flag Register */
-  __I  uint16_t IV;                                                              /*!< eUSCI_Ax Interrupt Vector Register */
-} EUSCI_A_Type;
-#endif
+
+#define RXBUF_CHAR_MASK 0xFF
+// ascii values used
+#define ASCII_ZERO 0x30
+#define ASCII_NINE 0x39
+#define ASCII_CR   0x0D
+#define ASCII_LF   0x0A
+// flag values
+#define READY_FLAG_READY 1
+#define READY_FLAG_NOT_READY 0
+// baud rate values
+#define BRW_FOR_3_MHZ 0x01
+#define BRF_FOR_3_MHZ 10
 // Initialize the UART
 void uart_init(void)
 {
@@ -31,8 +26,8 @@ void uart_init(void)
     EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_UCSSEL_2 // SMCLK
             | EUSCI_A_CTLW0_MODE_0 // UART mode
             | EUSCI_A_CTLW0_SWRST; // keep in reset
-    EUSCI_A0->BRW = 0x01; // from baud rate calculation
-    EUSCI_A0->MCTLW = (10 << EUSCI_A_MCTLW_BRF_OFS) //causing it to shift 4 bits as
+    EUSCI_A0->BRW = BRW_FOR_3_MHZ; // from baud rate calculation
+    EUSCI_A0->MCTLW = (BRF_FOR_3_MHZ << EUSCI_A_MCTLW_BRF_OFS) //causing it to shift 4 bits as
                         |EUSCI_A_MCTLW_OS16;      //offest from mctlw register
     P1->SEL0 |= (BIT2|BIT3);   // Configure EUSCI_A0
     P1->SEL1 &= ~(BIT2|BIT3); //TX and Rx pins
@@ -50,10 +45,8 @@ void uart_put_char(char chr)
     // wait for the Tx buffer to be ready
     while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG)) {
     }
-
     // buffer is free, write out data
     EUSCI_A0->TXBUF = chr;
-
 }
 
 // output a string
@@ -66,41 +59,31 @@ void uart_put_str(char * p_str)
 
     while (0 != *p_str) {
         uart_put_char(*p_str);
-
         p_str++;
     }
 }
-#if 0
-// get character from receive register
-char uart_get_char(void)
-{
-    // wait for the RX buffer to be ready
-    while(!(EUSCI_A0->IFG & EUSCI_A_IFG_RXIFG)) {
-    }
-    return (char)(EUSCI_A0->RXBUF & 0xFF); // cast receive buffer as char
-}
-#else
-static uint16_t last_value = 0;
-static uint8_t ready_to_read = 0;
+
+static uint16_t read_value = 0;
+static uint8_t ready_to_read = READY_FLAG_NOT_READY;
 uint16_t uart_get_value(void)
 {
     uint16_t tvalue;
     // wait for the RX buffer to be ready
-    while(ready_to_read != 1) {
+    while(ready_to_read != READY_FLAG_READY) {
     }
-    tvalue = last_value;
-    last_value = 0;
-    ready_to_read = 0;
+    tvalue = read_value;
+    ready_to_read = READY_FLAG_NOT_READY;
     return tvalue; // cast receive buffer as char
 }
-#endif
+
 // Basic receive IRQ handler
 void EUSCIA0_IRQHandler(void)
 {
+    static uint16_t isr_value = 0;
     char last_char;
     EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;  // Clear the RX interrupt flag
     // read the character
-    last_char = (char)(EUSCI_A0->RXBUF & 0xFF);
+    last_char = (char)(EUSCI_A0->RXBUF & RXBUF_CHAR_MASK);
     // echo it back
     // wait for the Tx buffer to be ready
     while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG)) {
@@ -108,15 +91,19 @@ void EUSCIA0_IRQHandler(void)
     // buffer is free, write out data
     EUSCI_A0->TXBUF = last_char;
     // parse character
-    if ((last_char >= 0x30) && (last_char <= 0x39)) {
-        last_value = (last_value * 10) + (last_char - 0x30);
+    if ((last_char >= ASCII_ZERO) && (last_char <= ASCII_NINE)) {
+        isr_value = (isr_value * 10) + (last_char - ASCII_ZERO);
     }
-    else if (last_char == 0xD) {
+    else if (last_char == ASCII_CR) {
         // wait for the Tx buffer to be ready
         while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG)) {
         }
         // buffer is free, write out data
-        EUSCI_A0->TXBUF = 0xA;
-        ready_to_read = 1;
+        EUSCI_A0->TXBUF = ASCII_LF;
+        if (isr_value != 0) {
+            read_value = isr_value;
+        }
+        isr_value = 0;
+        ready_to_read = READY_FLAG_READY;
     }
 }
