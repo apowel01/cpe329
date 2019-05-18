92b1ccae814febe80de8f20720af1769a67a268b
lorscorin <trevor@catsrock.net>
2019-05-18 11:20:27 -0700

    fix a couple of build errors

--- amberley_code/uart.c
+++ amberley_code/uart.c
@@ -6,7 +6,6 @@
  */
 #include "msp.h"
 #include "delay.h"
-#include "dac.h"
 
 #define RXBUF_CHAR_MASK 0xFF
 // ascii values used
@@ -152,23 +151,3 @@ void uart_put_num(uint32_t value)
     uart_put_str(string);
 }
 
-void uart_main(void)
-{
-    uint16_t inValue;
-    delay_set_dco(FREQ_3_0_MHz);
-
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-
-    dac_init();
-    uart_init();
-
-    while(1) {
-        inValue = uart_get_value();
-        if (inValue < 4096) {
-            dac_send(inValue);
-        }
-        else {
-            uart_put_str("ERROR: number out of range");
-        }
-    }
-}
