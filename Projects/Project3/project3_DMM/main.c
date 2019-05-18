f69f244b0b8899683cd2023175d48f4505f7edb1
apowel01 <44123994+apowel01@users.noreply.github.com>
2019-05-17 23:11:30 -0700

    friday night update

--- Projects/Project3/project3_DMM/main.c
+++ Projects/Project3/project3_DMM/main.c
@@ -1,4 +1,5 @@
 #include "msp.h"
+#include "delay.h"
 
 
 /**
@@ -8,12 +9,13 @@ void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-
-CS -> KEY = CS_KEY_VAL;    // Unlock CS register
-CS -> CTL1 |=  CS_CTL1_DIVHS_0    // set HSMCLK clock divider to 1
-		| CS_CTL1_DIVS_1        // set SMCLK clock divider to 2
-		| CS_CTL1_SELS_HFXTCLK;  // select HFXTCLK as clock source
-CS -> CTL2 |= CS_CTL2_HFXTFREQ_6 ;  // set HFXTCLK clock source to 40 - 48 MHz
-CS -> KEY = 0;   //  Locks CS Register
-
+// set the DCO clock and default SMCLK
+	delay_set_dco(FREQ_48_0_MHz);
+	// Setting values for SMCLK
+	CS->KEY = CS_KEY_VAL; // Unlock CS register
+	CS->CTL1 |= (CS_CTL1_DIVHS_0    	  // set HSMCLK clock divider to 1
+			    | CS_CTL1_DIVS_1          // set SMCLK clock divider to 2
+				| CS_CTL1_SELS_HFXTCLK);  // select HFXTCLK as clock source
+//	CS->CTL2 |= CS_CTL2_HFXTFREQ_6 ; 	  // set HFXTCLK clock source to 40 - 48 MHz
+	CS->KEY = 0; //  Locks CS Register
 }
